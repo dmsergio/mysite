04ed9da93caecb3b991df88eca0fc92e4afe6ef1
dmsergio <sergiodm.1989@gmail.com>
2020-10-04 18:40:53 +0200

    Add templates to visualize list blog and detail blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request=request,
+        template_name='blog/post/list.html',
+        context={'posts': posts}
+    )
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(
+        request=request,
+        template_name='blog/post/detail.html',
+        context={'post': post}
+    )
