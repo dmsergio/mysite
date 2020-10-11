f0847561d5e9da4c433a3b9c816b014dede3b125
dmsergio <sergiodm.1989@gmail.com>
2020-10-11 17:21:13 +0200

    Add comments to the blog

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 
-from .models import Post
-from .form import EmailPostForm
+from .models import Comment, Post
+from .form import CommentForm, EmailPostForm
 
 
 class PostListView(ListView):
@@ -43,10 +43,27 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(
         request=request,
         template_name='blog/post/detail.html',
-        context={'post': post}
+        context={
+            'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form,
+        }
     )
 
 def post_share(request, post_id):
