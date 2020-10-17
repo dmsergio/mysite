3e8fd35868d9c2b15bf15fdc918c136fc5d1eadd
dmsergio <sergiodm.1989@gmail.com>
2020-10-17 17:52:00 +0200

    Add more custom tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from ..models import Post
 
 
@@ -7,3 +9,13 @@ register = template.Library()
 @register.simple_tag(name='total_posts')
 def get_total_published_posts():
     return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    posts = Post.published.annotate(total_comments=Count('comments'))
+    return posts.order_by('-total_comments')[:count]
